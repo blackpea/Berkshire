73e8a4be31d150d7a8e07b27b708e52dbb689d37
blackpea <liuchiahuan@gmail.com>
2014-06-18 00:25:04 +0800

    upload雛形
    upload雛形

---
+++ html5up-dopetrope-mvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace html5up_dopetrope_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Dopetrope by HTML5 UP";
+            return View();
+        }
+
+        public ActionResult LeftSidebar()
+        {
+            ViewBag.Title = "Left Sidebar - Dopetrope by HTML5 UP";
+            return View();
+        }
+
+        public ActionResult NoSidebar()
+        {
+            ViewBag.Title = "No Sidebar - Dopetrope by HTML5 UP";
+            return View();
+        }
+
+        public ActionResult RightSidebar()
+        {
+            ViewBag.Title = "Right Sidebar - Dopetrope by HTML5 UP";
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ html5up-dopetrope-mvc/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using html5up_dopetrope_mvc.App_Start;
+
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace html5up_dopetrope_mvc
+{
+    using Owin;
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
